a819c2a6b13f9e4998df8d118f6104fb7a041aa9
Thiago Emanoel <thiagoemanoel448@gmail.com>
2022-05-23 21:34:11 -0300

    Configuração inicial

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Home } from './src/pages/Home';
+
+const App: React.FC = () => {
+    return <Home />;
+};
+
+export default App;
