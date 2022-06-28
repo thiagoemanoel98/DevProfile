a64ed905bfbb11fd3da51bce8601250cbc38edfd
Thiago Emanoel <thiagoemanoel448@gmail.com>
2022-06-27 23:08:10 -0300

    Refact: SignIn e SignUp com o hook-form

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+
 import { ScrollView, KeyboardAvoidingView, Platform, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { useForm, FieldValues } from 'react-hook-form';
+
 import { Button } from '../../components/Form/Button';
-import { Input } from '../../components/Form/Input';
+
 import {
     Container,
     Content,
@@ -15,13 +18,29 @@ import {
     Title,
 } from './styles';
 import logo from '../../assets/logo.png';
+import { InputControl } from '../../components/Form/InputControl';
 
 interface ScreenNavigationProp {
     navigate: (screen: string) => void;
 }
 
+// INterface genérica
+// Recebe qualquer coisa, desde que seja uma string
+interface IFormInputs {
+    [name: string]: any;
+}
+
 export const SignIn: React.FunctionComponent = () => {
+    const { handleSubmit, control } = useForm<FieldValues>();
     const navigation = useNavigation<ScreenNavigationProp>();
+
+    const handleSignIn = (form: IFormInputs) => {
+        const Data = {
+            email: form.email,
+            password: form.password,
+        };
+        console.log(Data);
+    };
 
     return (
         // Fecha o teclado se o usuário tocar fora do teclado
@@ -40,10 +59,27 @@ export const SignIn: React.FunctionComponent = () => {
                         <View>
                             <Title>Faça o Login</Title>
                         </View>
-                        <Input placeholder="Email" />
-                        <Input placeholder="Senha" />
+                        <InputControl
+                            autoCapitalize="none"
+                            autoCorrect={false}
+                            control={control}
+                            name="email"
+                            placeholder="Email"
+                            keyboardType="email-address"
+                        />
+                        <InputControl
+                            control={control}
+                            autoCapitalize="none"
+                            autoCorrect={false}
+                            name="senha"
+                            placeholder="Senha"
+                            secureTextEntry
+                        />
 
-                        <Button title="Entrar" />
+                        <Button
+                            title="Entrar"
+                            onPress={handleSubmit(handleSignIn)}
+                        />
 
                         <ForgotPasswordButton>
                             <ForgotPasswordTitle>
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { useForm, FieldValues } from 'react-hook-form';
 import { KeyboardAvoidingView, Platform, ScrollView } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { Button } from '../../components/Form/Button';
-import { Input } from '../../components/Form/Input';
+import { InputControl } from '../../components/Form/InputControl';
 import {
     BackToSignIn,
     BackToSignInTitle,
@@ -18,8 +19,24 @@ interface ScreenNavigationProp {
     goBack: () => void;
 }
 
+// INterface genérica
+// Recebe qualquer coisa, desde que seja uma string
+interface IFormInputs {
+    [name: string]: any;
+}
+
 export const SignUp: React.FunctionComponent = () => {
+    const { handleSubmit, control } = useForm<FieldValues>();
     const navigation = useNavigation<ScreenNavigationProp>();
+
+    const handleSignUp = (form: IFormInputs) => {
+        const Data = {
+            name: form.name,
+            email: form.email,
+            password: form.password,
+        };
+        console.log(Data);
+    };
 
     return (
         // Fecha o teclado se o usuário tocar fora do teclado
@@ -36,11 +53,35 @@ export const SignUp: React.FunctionComponent = () => {
                     <Content>
                         <Logo source={logo} />
                         <Title>Crie a sua conta</Title>
-                        <Input placeholder="Nome completo" />
-                        <Input placeholder="Email" />
-                        <Input placeholder="Senha" />
+                        <InputControl
+                            autoCapitalize="words"
+                            autoCorrect={false}
+                            control={control}
+                            name="name"
+                            placeholder="Nome completo"
+                        />
 
-                        <Button title="Criar conta" />
+                        <InputControl
+                            autoCapitalize="none"
+                            autoCorrect={false}
+                            control={control}
+                            name="email"
+                            placeholder="Email"
+                            keyboardType="email-address"
+                        />
+                        <InputControl
+                            control={control}
+                            autoCapitalize="none"
+                            autoCorrect={false}
+                            name="password"
+                            placeholder="Senha"
+                            secureTextEntry
+                        />
+
+                        <Button
+                            title="Criar conta"
+                            onPress={handleSubmit(handleSignUp)}
+                        />
                     </Content>
                 </Container>
             </ScrollView>
